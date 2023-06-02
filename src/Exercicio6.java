8a4c54b288700cfda8ad5ebabf780d6377442271
joaocruzzup <133882160+joaocruzzup@users.noreply.github.com>
2023-06-02 13:39:14 -0300

    :sparkles: feat: Adicionado ex06

---
+++ src/Exercicio6.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+// Crie um programa para verificar se o inteiro fornecido é um múltiplo de 5 e 7.
+public class Exercicio6 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Digite um número inteiro: ");
+        int num = sc.nextInt();
+
+        if (num % 5 == 0 && num %7 ==0){
+            System.out.printf("O número %d é múltiplo de 5 e 7", num);
+        } else if (num % 5 == 0) {
+            System.out.printf("O número %d é múltiplo apenas de 5", num);
+        } else if (num % 7 == 0){
+            System.out.printf("O número %d é múltiplo apenas de 7", num);
+        } else {
+            System.out.println("O número não é múltiplo nem de 5 nem de 7");
+        }
+
+    }
+}
