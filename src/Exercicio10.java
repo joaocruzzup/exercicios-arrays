9a690bc0490f4afb0b015fa193cbec0665f98eeb
joaocruzzup <133882160+joaocruzzup@users.noreply.github.com>
2023-06-02 17:34:41 -0300

    :sparkles: feat: Adicionado ex10

--- src/Exercicio10.java
+++ src/Exercicio10.java
@@ -1,2 +1,23 @@
+import java.util.Arrays;
+import java.util.List;
+
+// Criar um vetor A com 10 elementos inteiros. Implementar um programa que defina e escreva a média aritmética simples
+// dos elementos ímpares armazenados neste vetor.
 public class Exercicio10 {
+    public static void main(String[] args) {
+        int[] vetor = new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+
+        System.out.println("A lista dos números é: " + Arrays.toString(vetor));
+        int qtdImpares = 0;
+        int somaImpares = 0;
+        for (int numero: vetor) {
+            if ((numero % 2) != 0){
+                somaImpares += numero;
+                qtdImpares += 1;
+            }
+        }
+        System.out.printf("A soma dos números ímpares presentes no vetor é: %d", somaImpares);
+
+
+    }
 }
