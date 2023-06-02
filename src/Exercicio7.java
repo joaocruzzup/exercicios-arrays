fb581271df94c5af06399663f943d875091a95e7
joaocruzzup <133882160+joaocruzzup@users.noreply.github.com>
2023-06-02 16:20:18 -0300

    :sparkles: feat: Adicionado ex07

---
+++ src/Exercicio7.java
@@ -0,0 +1,24 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Scanner;
+
+// Crie um programa para exibir o inteiro fornecido pelo usuário na ordem inversa.
+public class Exercicio7 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        List<Integer> lista_numeros = new ArrayList<>();
+        System.out.println("Quantos números você deseja inserir na lista: ");
+        int n = sc.nextInt();
+
+        for (int i = 1; i <= n; i++){
+            System.out.printf("Digite o %dª número: ", i);
+            lista_numeros.add(sc.nextInt());
+        }
+
+        Collections.reverse(lista_numeros);
+        System.out.println("A lista na ordem inversa da digitada é: ");
+        System.out.println(lista_numeros);
+
+    }
+}
