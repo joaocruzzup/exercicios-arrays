70edd9315d41804c5cdd93529261a2fcc2ee5e68
joaocruzzup <133882160+joaocruzzup@users.noreply.github.com>
2023-06-02 13:30:14 -0300

    :sparkles: feat: Atualizado Ex4

--- src/Exercicio4.java
+++ src/Exercicio4.java
@@ -1,4 +1,4 @@
-//Tem-se um conjunto de dados contendo a altura e o sexo (masculino, feminino) de 10 pessoas. Fazer um algoritmo que calcule e escreva:
+// Tem-se um conjunto de dados contendo a altura e o sexo (masculino, feminino) de 10 pessoas. Fazer um algoritmo que calcule e escreva:
 // a maior e a menor altura do grupo;
 // b. média de altura dos homens;
 // c. o número de mulheres.
